62e6650aaf89f45c9cf7aae36a94163971e7f656
Louis DONDEY <151539303+Kae134@users.noreply.github.com>
2026-05-14 14:51:09 +0200

    Added button + some test + config file (#2)

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,60 @@
-#include <Arduino.h>
-#include <Config.h>
-#include <WiFi.h>
-#include <WiFiConnection.h>
-#include <MQTT.h>
+  #include <Arduino.h>
+  #include <Config.h>
+  #include <WiFi.h>
+  #include <WiFiConnection.h>
+  #include <MQTT.h>
+  #include <Button.h>
 
-WiFiConnection wifi(WIFI_SSID, WIFI_PASSWORD, WIFI_MAX_CONNECTION_RETRY);
-MQTT mqtt(
-    "esp32-test",
-    "192.168.1.xx",
-    "",
-    "",
-    "home/esp32/status",
-    1883
-);
+  // WiFiConnection wifi(WIFI_SSID, WIFI_PASSWORD, WIFI_MAX_CONNECTION_RETRY);
+  // MQTT mqtt(
+  //     "esp32-test",
+  //     "192.168.1.xx",
+  //     "",
+  //     "",
+  //     "home/esp32/status",
+  //     1883
+  // );
 
-void setup() {
-  Serial.begin(115200);
-  delay(200);
+  Button buttonTest(BUTTON_PIN);
+  
+  void onButtonClick() {
+    Serial.printf("Click");
+  }
 
-  wifi.begin();
+  void onButtonDoubleClick() {
+    Serial.printf("DoubleClick");
+  }
 
-  Serial.println(WiFi.localIP());
+  void onButtonPress() {
+    Serial.printf("Press");
+  }
 
-  mqtt.begin();
-}
+  void onLongPress() {
+    Serial.printf("Long Press");
+  }
 
-void loop() {
-  mqtt.loop();
-}
+  void onRelease() {
+    Serial.printf("Release");
+  }
+  
+  void setup() {
+    Serial.begin(115200);
+    delay(200);
+
+    // wifi.begin();
+
+    // Serial.println(WiFi.localIP());
+
+    // mqtt.begin();
+
+    buttonTest.begin();
+    buttonTest.onClick(onButtonClick);
+    buttonTest.onDoubleClick(onButtonDoubleClick);
+    buttonTest.onLongPress(onLongPress);
+    buttonTest.onRelease(onRelease);
+  }
+
+  void loop() {
+    // mqtt.loop();
+    buttonTest.update();
+  }
